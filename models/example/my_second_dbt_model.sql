3bae4460a998082ff3ab9d54302b8bb11a8e727f
Lahoucine EL AIDOUS <elaidous.lahoucine@gmail.com>
2021-09-23 21:41:52 +0000

    demo added

--- models/example/my_second_dbt_model.sql
+++ models/example/my_second_dbt_model.sql
@@ -1,6 +1,11 @@
 
 -- Use the `ref` function to select from other models
 
-select *
-from {{ ref('my_first_dbt_model') }}
-where id = 1
+SELECT 
+    start_station_name, 
+    end_station_name, 
+    count(*) trips
+FROM 
+    {{ ref('my_first_dbt_model') }}
+GROUP BY 1,2 
+ORDER BY 3 desc   
